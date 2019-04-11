0ab2084bce59aeaa502430cfea74b745d758cf9d
Kylep342 <kylep342@gmail.com>
2019-04-11 08:23:07 -0500

    Renamed component Letter -> Tile Added styling to approach true design.

--- src/index.js
+++ src/index.js
@@ -1,105 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-
-class Letter extends React.Component {
-    render() {
-        const letter = this.props.value.toUpperCase();
-        const pointValues = {
-            "A": 1,
-            "B": 3,
-            "C": 3,
-            "D": 2,
-            "E": 1,
-            "F": 4,
-            "G": 2,
-            "H": 4,
-            "I": 1,
-            "J": 8,
-            "K": 5,
-            "L": 1,
-            "M": 3,
-            "N": 1,
-            "O": 1,
-            "P": 3,
-            "Q": 10,
-            "R": 1,
-            "S": 1,
-            "T": 1,
-            "U": 1,
-            "V": 4,
-            "W": 4,
-            "X": 8,
-            "Y": 4,
-            "Z": 10
-        }
-        const score = pointValues[letter.toUpperCase()];
-        return (
-            <div>
-                <div className="letter">
-                    {this.props.value}
-                </div>
-                <div>
-                    {score}
-                </div>
-            </div>
-            
-        );
-    }
+class Tile extends React.Component {
+  render() {
+    const letter = this.props.value;
+    const pointValues = {
+      A: 1,
+      B: 3,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 4,
+      Z: 10
+    };
+    const score = pointValues[letter.toUpperCase()];
+    return (
+      <div className="Tile">
+        <div className="letter">
+          {this.props.value}
+          <sub className="pointValue">{score}</sub>
+        </div>
+      </div>
+    );
+  }
 }
 
 class Word extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    renderLetter(letter, index) {
-        return <Letter value={letter} key={index} />;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+  renderTile(letter, index) {
+    return <Tile value={letter.toUpperCase()} key={index} />;
+  }
 
-    render() {
-        const letters = this.props.value.split('').map((letter, i) => {
-            return this.renderLetter(letter, i);
-        });
-        
-        return (
-            <div>
-                {letters}
-            </div>
-        );
-    }
+  render() {
+    const letters = this.props.value.split("").map((letter, i) => {
+      return this.renderTile(letter, i);
+    });
+
+    return <div className="Word">{letters}</div>;
+  }
 }
 
 class InputBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
 
-        this.handleChange = this.handleChange.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-    renderWord(word) {
-        return <Word value={word} />;
-    }
+  renderWord(word) {
+    return <Word value={word} />;
+  }
 
-    render() {
-        return (
-            <div>
-                <input type="text" onChange={this.handleChange}></input>
-                <Word value={this.state.value} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="center">
+        <div className="center half">
+          <input
+            className="InputBox"
+            type="text"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="center">
+          <Word value={this.state.value} />
+        </div>
+      </div>
+    );
+  }
 }
 
-
-ReactDOM.render(
-    <InputBox value={''} />,
-    document.getElementById('root')
-);
+ReactDOM.render(<InputBox value={""} />, document.getElementById("root"));
