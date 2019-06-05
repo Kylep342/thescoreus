26eef19db4198bec072027188e4f798977f981f2
Kylep342 <kylep342@gmail.com>
2019-06-05 08:18:59 -0600

    Added a word score component

--- src/index.js
+++ src/index.js
@@ -2,37 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const pointValues = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10
+};
+
 class Tile extends React.Component {
   render() {
     const letter = this.props.value;
-    const pointValues = {
-      A: 1,
-      B: 3,
-      C: 3,
-      D: 2,
-      E: 1,
-      F: 4,
-      G: 2,
-      H: 4,
-      I: 1,
-      J: 8,
-      K: 5,
-      L: 1,
-      M: 3,
-      N: 1,
-      O: 1,
-      P: 3,
-      Q: 10,
-      R: 1,
-      S: 1,
-      T: 1,
-      U: 1,
-      V: 4,
-      W: 4,
-      X: 8,
-      Y: 4,
-      Z: 10
-    };
     const score = pointValues[letter.toUpperCase()];
     return (
       <div className="Tile">
@@ -55,13 +56,32 @@ class Word extends React.Component {
   renderTile(letter, index) {
     return <Tile value={letter.toUpperCase()} key={index} />;
   }
+  renderWordScore(letters) {
+    let total = 0;
+    for (let letter of letters) {
+      total += pointValues[letter.props.value];
+    }
+    return total;
+  }
 
   render() {
     const letters = this.props.value.split("").map((letter, i) => {
       return this.renderTile(letter, i);
     });
+    const wordScore = this.renderWordScore(letters);
 
-    return <div className="Word">{letters}</div>;
+    return (
+      <div>
+        <div className="Word">{letters}</div>
+        <div className="WordScore">
+          {isNaN(wordScore)
+            ? "Please enter a single word without spaces."
+            : wordScore
+            ? `Word Score: ${wordScore}`
+            : ""}
+        </div>
+      </div>
+    );
   }
 }
 
